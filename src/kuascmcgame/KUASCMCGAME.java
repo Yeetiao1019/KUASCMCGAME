f0a0029fab2a8eb7f8449d6cf26ad24a28b66748
Yee Chen <n1533512@gmail.com>
2017-08-09 01:24:38 +0800

    v0.2

--- src/kuascmcgame/KUASCMCGAME.java
+++ src/kuascmcgame/KUASCMCGAME.java
@@ -8,18 +8,86 @@ import java.awt.*;
  */
 public class KUASCMCGAME extends JFrame {
 
+    KUASCMCGAME(){
+        Container container = getContentPane();
+        container.setLayout(new GridLayout(2,1));
+       // container.setSize(new Dimension(650,500));
+       // container.setLayout(new FlowLayout());
+        ImageIcon kuas = new ImageIcon(KUASCMCGAME.class.getResource("kuas.png"));
+        ImageIcon cmc = new ImageIcon(KUASCMCGAME.class.getResource("cmc.png"));
+        JLabel kuasimg  = new JLabel(kuas);
+        JLabel cmcimg  = new JLabel(cmc);
+        JPanel difficulty = new JPanel();
+        JPanel word = new JPanel();
+        
+        word.setLayout(new BorderLayout());
+        word.setBorder(BorderFactory.createLineBorder(Color.blue, 2));
+        difficulty.setLayout(null);
+        difficulty.setBorder(BorderFactory.createLineBorder(Color.yellow, 2));
+        JButton easy = new JButton("簡單");
+        JButton normal = new JButton("中等");
+        JButton hard = new JButton("困難");
+        
+        easy.setSize(new Dimension(100,50));
+        easy.setLocation(100,80);
+        normal.setSize(new Dimension(100,50));
+        normal.setLocation(300,80);
+        hard.setSize(new Dimension(100,50));
+        hard.setLocation(500,80);
+        
+        int kuaswidth = 350;
+        int kuasheight = 69;
+        int cmcwidth = 200;
+        int cmcheight = 70;
+        kuas.setImage(kuas.getImage().getScaledInstance(kuaswidth,kuasheight,Image.SCALE_DEFAULT)); 
+        kuasimg.setSize(new Dimension(kuaswidth,kuasheight));
+        kuasimg.setLocation(50,50);
+        cmc.setImage(cmc.getImage().getScaledInstance(cmcwidth,cmcheight,Image.SCALE_DEFAULT)); 
+        cmcimg.setSize(new Dimension(cmcwidth,cmcheight));
+        cmcimg.setLocation(100,100);
+       /*
+        JButton bowstring = new JButton("拉弦");
+        JButton wind = new JButton("吹管");
+        JButton pluck = new JButton("彈撥");
+        JButton percussion = new JButton("打擊");
+        
+        bowstring.setSize(new Dimension(100,50));
+        bowstring.setLocation(20,20);
+        wind.setSize(new Dimension(100,50));
+        wind.setLocation(20,75);
+        pluck.setSize(new Dimension(100,50));
+        pluck.setLocation(20,130);
+        percussion.setSize(new Dimension(100,50));
+        percussion.setLocation(20,185);
+        
+        difficulty.add(bowstring);
+        difficulty.add(wind);
+        difficulty.add(pluck);
+        difficulty.add(percussion);
+        */
+        difficulty.add(easy);
+        difficulty.add(normal);
+        difficulty.add(hard);
+        word.add(kuasimg,BorderLayout.CENTER);
+        word.add(cmcimg,BorderLayout.SOUTH);
+        container.add(word);
+        container.add(difficulty);
+        
+        setTitle("KUASCMC Music Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(650,500);
+        setLocationRelativeTo(null);
+        
+        
+        
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
-        JFrame Frame = new JFrame("KUASCMC Music Game");
-        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Frame.setVisible(true);
-        Frame.setSize(650,500);
-        Frame.setLocationRelativeTo(null);
 
-        */
+        new KUASCMCGAME();
         
     }
     
